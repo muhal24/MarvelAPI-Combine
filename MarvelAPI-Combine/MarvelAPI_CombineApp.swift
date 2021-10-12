2323847058cccd03599bd7b2a31cea3f97fd6008
muhal24 <hanifi.alma2014.mha@gmail.com>
2021-10-12 16:51:27 +0300

    Initial Commit

---
+++ MarvelAPI-Combine/MarvelAPI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPI_CombineApp.swift
+//  MarvelAPI-Combine
+//
+//  Created by Muhammed Hanifi Alma on 12.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
